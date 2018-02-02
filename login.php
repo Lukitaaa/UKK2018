edfe2c19213ea90d96fff2e35f6109380fbbde3e
Lukitaaa <lukitalita9@gmail.com>
2018-02-02 07:29:29 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> LOGIN </title>
+</head>
+<body>
+	<form action="login.php" method="post">
+		<label>Username : </label>
+		<input type="text" name="Username" id="Username"></input>
+		<p>
+		<label>Password : </label>
+		<input type="Password" name="Password" id="Password"></input>
+		<p>
+		<input type="submit" name="login" class="tombol">
+	</form>
+</body>
+</html>
